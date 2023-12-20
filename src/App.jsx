f40870bb07e392b3bb3df30940e976e9ca029afa
bodin <bodinsonsawan@gmail.com>
2023-12-21 00:00:38 +0700

    bodin's assignment edited

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        <MoviesList />
         <h1 className="movie-list-header">Movie List Section</h1>
+        <MoviesList />
       </section>
     </div>
   );
