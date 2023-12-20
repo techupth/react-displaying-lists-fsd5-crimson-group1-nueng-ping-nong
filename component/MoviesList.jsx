9c88d5b7dde326fb785d651175f42ca95d989d2a
bodin <bodinsonsawan@gmail.com>
2023-12-20 22:35:52 +0700

    bodin's assignment

---
+++ component/MoviesList.jsx
@@ -0,0 +1,83 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import movies from "../src/data/movies";
+
+function MoviesList() {
+  return (
+    <div>
+      {movies.map((item, index) => {
+        return (
+          <div
+            key={index}
+            css={css`
+              display: flex;
+              background-color: #ffffff;
+              height: 250px;
+              width: 400px;
+              border-radius: 10px;
+              padding: 18px 14px 18px 14px;
+              box-shadow: 0px 4px 40px rgba(145, 124, 124, 0.25);
+              margin-bottom: 80px;
+              color: #181818;
+              font-family: kanit;
+            `}
+          >
+            <div>
+              <img
+                src={item.image}
+                css={css`
+                  width: 102px;
+                  height: 100px;
+                  object-fit: cover;
+                  border-radius: 10px;
+                `}
+              ></img>
+            </div>
+            <div
+              css={css`
+                margin-left: 20px;
+                font-size: 20px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+              `}
+            >
+              <div>Title: {item.title}</div>
+              <div>Year: {item.year}</div>
+              <div>Runtime: {item.runtime}</div>
+              <div
+                css={css`
+                  display: flex;
+                `}
+              >
+                Genres:
+                {item.genres.map((genre, i) => {
+                  return (
+                    <div
+                      key={i}
+                      css={css`
+                        background-color: #eaac99;
+                        border-radius: 10px;
+                        font-size: 12px;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                        margin-left: 10px;
+                        margin-top: 5px;
+                        color: white;
+                      `}
+                    >
+                      {genre}
+                    </div>
+                  );
+                })}
+              </div>
+              <div>IMDB Ratings: {item.imdbRating}</div>
+              <div>IMDB Votes: {item.imdbVotes}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default MoviesList;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import MoviesList from "../component/MoviesList";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <MoviesList />
+        <h1 className="movie-list-header">Movie List Section</h1>
       </section>
     </div>
   );
